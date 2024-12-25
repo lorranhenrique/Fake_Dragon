c5dfc2b69950a7ddb79af2534d50cf635c71219b
Lorran Henrique Rezende Alves <72421034+lorranhenrique@users.noreply.github.com>
2024-12-25 17:59:24 -0300

    fix:Ajuste na função com a cheve errada

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,16 +1,17 @@
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Bullet2 : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
     public GameObject blow;
     private CircleCollider2D circle;
     private SpriteRenderer sr;
     private Rigidbody2D rb;
     public ParticleSystem smoke;
-    public static Bullet2 Instance;
+    public static Bullet Instance;
     public int damage;
     public float acelerator;
+    public int atirador;
 
 
     void Start()
@@ -45,7 +46,15 @@ public class Bullet2 : MonoBehaviour
 
     private void Update()
     {
-        rb.linearVelocity += new Vector2(acelerator * Time.deltaTime * Player2.Instance.direction, 0f);
+        if(atirador == 1)
+        {
+            rb.linearVelocity += new Vector2(acelerator * Time.deltaTime * Player.Instance.direction, 0f);
+        }
+        else
+        {
+            rb.linearVelocity += new Vector2(acelerator * Time.deltaTime * Player2.Instance.direction, 0f);
+        }
+        
 
     }
 
