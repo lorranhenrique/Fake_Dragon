f833555fec7bf02d7cb988f531b9c6bf54c7e196
unknown <logplayviews@gmail.com>
2025-03-14 13:54:22 -0300

    Fix: Ajuste no conflito do ShaderGraph

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,7 +5,6 @@ using Photon.Pun;
 using Photon.Pun.UtilityScripts;
 using Photon.Realtime;
 using UnityEditor;
-using UnityEditor.ShaderGraph;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
